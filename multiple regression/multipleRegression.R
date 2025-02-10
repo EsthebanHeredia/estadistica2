bbf8ffaa1daa2f587e2409221cf53e442792abb2
Estheban Heredia <esthebanheredia15@gmail.com>
2025-02-10 09:51:41 -0600

    Update multipleRegression.R

--- multiple regression/multipleRegression.R
+++ multiple regression/multipleRegression.R
@@ -10,3 +10,15 @@ model2 = lm(Travel_time ~ Miles + Deliveries, data = mydata)
 summary(model2)
 model2$residuals
 cor(mydata)
+
+newdata = data.frame(Deliveries = c(5), Miles = c(90))
+newdata
+
+est = predict(model2, newdata =newdata)
+est
+
+conf = predict(model2, newdata = newdata, interval = "confidence")
+conf
+
+conf = predict(model2, newdata = newdata, interval = "predict")
+conf
