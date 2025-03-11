97ac42560a12ac5b6febd800e583fea9f78e0ec9
San231926 <san231926@uvg.edu.gt>
2025-03-10 20:56:19 -0600

    Microproyecto2 Markdown

--- microproyecto2/microproyecto2.R
+++ microproyecto2/microproyecto2.R
@@ -6,10 +6,10 @@ library(tidyr)
 mydata = read_xlsx("microproyecto2.xlsx")
 mydata
 
-arranged_data = pivot_longer(mydata, cols = c("1","2","3"), names_to = "Estudiante", values_to="Valores")
+arranged_data = pivot_longer(mydata, cols = "1":"3", names_to = "Metodo", values_to="Tiempo")
 arranged_data
 
-results = aov(Valores ~ Estudiante + ...1, data = arranged_data)
+results = aov(Tiempo ~ Metodo + Estudiante, data = arranged_data)
 summary(results)
 
 lsdresults = LSD.test(results, "Estudiante")
